0a0857638e6cd149f57cd2b11a1ee414942024ce
Sovannak22 <beng.sovannak@gmail.com>
2020-03-11 15:43:53 +0700

    Add new configuration environment file for testing with H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1,sysdate(),'test1@gmail.com','AB','+855012345678')
-insert into user values(2,sysdate(),'test2@gmail.com','ABC','+855012345678')
-insert into user values(3,sysdate(),'test3@gmail.com','ABD','+855012345678')
+INSERT INTO roles VALUES(1,'Super Admin')
+INSERT INTO roles VALUES(2,'Admin')
+INSERT INTO roles VALUES(3,'User')
